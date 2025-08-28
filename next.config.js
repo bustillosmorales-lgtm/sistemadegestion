3236874b04fe721ff47a14f8c4088ea5b08dc445
Fran <bustillosmorales@gmail.com>
2025-08-28 16:22:37 -0400

    Cambio a SSR para soporte de API routes

--- next.config.js
+++ next.config.js
@@ -1,11 +1,8 @@
 ﻿/** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
   trailingSlash: true,
   images: {
     unoptimized: true
-  },
-  distDir: 'out'
+  }
 }
-
 module.exports = nextConfig
